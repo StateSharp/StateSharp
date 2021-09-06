f39a5f91b767d93e163c1f9310d0e1efaa7ab553
Hrafn Orri Hrafnkelsson <HrafnOrri@BitCrow.net>
2021-09-06 17:48:02 +0000

    coverage

--- bin/ci/dotnet/test.sh
+++ bin/ci/dotnet/test.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 
 dotnet test --collect:"XPlat Code Coverage" --settings:coverlet.runsettings
 wget https://github.com/codecov/codecov-bash/releases/download/1.0.5/codecov
+
+find ./tests/UnitTests/TestResults/
+
 bash codecov -f $(find ./tests/UnitTests/TestResults/)
 
 exit 0
