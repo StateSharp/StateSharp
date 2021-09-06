0f80af8daa6ca665f78957138399960ca4d0b9f4
Hrafn Orri Hrafnkelsson <HrafnOrri@BitCrow.net>
2021-09-06 16:49:31 +0000

    test

--- bin/ci/dotnet/test.sh
+++ bin/ci/dotnet/test.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
-dotnet test --no-build --verbosity normal
+wget -O ./opencover.zip https://github.com/OpenCover/opencover/releases/download/4.7.1221/opencover.4.7.1221.zip
+unzip opencover.zip -d ./bin/opencover
+find ./bin/opencover
+
+# dotnet test --no-build --verbosity normal
 
 exit 0
