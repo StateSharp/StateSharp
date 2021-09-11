1ffd8c17866ca2fc91c82f14e5312567941173f7
Hrafn Orri Hrafnkelsson <HrafnOrri@BitCrow.net>
2021-09-11 11:33:36 +0000

    generator

---
+++ src/Generator/MyGenerator.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace StateSharp.Generator
+{
+    [Generator]
+    public class MyGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var source = "class Foo { }";
+
+            if (source != null)
+            {
+                context.AddSource("generated.cs", source);
+            }
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+
+        }
+    }
+}
---
+++ tests/GeneratedApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GeneratedApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo = new Foo();
+            Console.WriteLine("Types in this assembly:");
+            foreach (Type t in typeof(Program).Assembly.GetTypes())
+            {
+                Console.WriteLine(t.FullName);
+            }
+        }
+    }
+}
