0dfaa0b665fb320c0a0ec78093aded5ef661bbf7
Hrafn Orri Hrafnkelsson <HrafnOrri@BitCrow.net>
2021-09-20 17:59:02 +0000

    models

---
+++ src/Generator/Models/FieldModel.cs
@@ -0,0 +1,14 @@
+﻿namespace StateSharp.Generator.Models
+{
+    public class FieldModel
+    {
+        public string Name { get; }
+        public TypeModel Type { get; }
+
+        public FieldModel(string name, TypeModel type)
+        {
+            Name = name;
+            Type = type;
+        }
+    }
+}
---
+++ src/Generator/Models/TypeModel.cs
@@ -0,0 +1,14 @@
+﻿namespace StateSharp.Generator.Models
+{
+    public class TypeModel
+    {
+        public string Name { get; }
+        public string Namespace { get; }
+
+        public TypeModel(string name, string @namespace)
+        {
+            Name = name;
+            Namespace = @namespace;
+        }
+    }
+}
